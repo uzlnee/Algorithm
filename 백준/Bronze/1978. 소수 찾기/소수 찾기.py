57a38de1c333742bb12afe60aef4da0697574a6f
Jeong yujin <105401719+uzlnee@users.noreply.github.com>
2024-12-19 21:50:52 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -3,12 +3,10 @@ lst = list(map(int, input().split()))
 
 cnt = 0
 for i in lst:
-    rmd = []
-    if i > 1:
-        for a in range(2, i):
-            b = i % a
-            rmd.append(b)
-        if 0 not in rmd:
-            cnt += 1
+    for a in range(2, i+1):
+        if i % a == 0:
+            if i == a:
+                cnt += 1
+            break
 
 print(cnt)
